0843b27166e9798d1561f51cdfbbf8167775f77a
moriss_h <moriss_h@epitech.eu>
2014-03-29 20:02:39 +0100

    better color handling for sdl

--- lib_sdl/src/Graphique.cpp
+++ lib_sdl/src/Graphique.cpp
@@ -2,7 +2,29 @@
 
 Graphique::Graphique()
 {
+  std::string	error;
 
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+      error = "Can't Init SDL: ";
+      error += SDL_GetError();
+      throw nFault(error, true);
+    }
+  _colorMap[APPLE].push_back(0);
+  _colorMap[APPLE].push_back(255);
+  _colorMap[APPLE].push_back(0);
+
+  _colorMap[SNAKE].push_back(128);
+  _colorMap[SNAKE].push_back(128);
+  _colorMap[SNAKE].push_back(0);
+
+  _colorMap[WALL].push_back(255);
+  _colorMap[WALL].push_back(0);
+  _colorMap[WALL].push_back(0);
+
+  _colorMap[EMPTY].push_back(0);
+  _colorMap[EMPTY].push_back(0);
+  _colorMap[EMPTY].push_back(0);
 }
 
 Graphique::~Graphique()
@@ -15,12 +37,11 @@ void Graphique::createWindows(const Point2d<int>& size)
 {
   std::string	error;
 
-  if ((SDL_Init(SDL_INIT_VIDEO) != 0)
-      || ((_win = SDL_CreateWindow("Nibbler", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                                   size.x(), size.y(), SDL_WINDOW_SHOWN)) == NULL)
+  if (((_win = SDL_CreateWindow("Nibbler", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                size.x(), size.y(), SDL_WINDOW_SHOWN)) == NULL)
       || ((_rend = SDL_CreateRenderer(_win, -1, SDL_RENDERER_ACCELERATED)) == NULL))
     {
-      error = "Can't Init SDL: ";
+      error = "SDL can't create window: ";
       error += SDL_GetError();
       throw nFault(error, true);
     }
@@ -28,27 +49,23 @@ void Graphique::createWindows(const Point2d<int>& size)
   SDL_RenderPresent(_rend);
 }
 
-//APPLE, SNAKE, WALL, EMPTY
 void Graphique::drawSquare(const Box<int>& square, blockType type)
 {
   SDL_Rect	pos;
 
-  (void) type;
+  std::vector<unsigned char>& colors = _colorMap.at(type);
   pos.x = square.getPos().x();
   pos.y = square.getPos().y();
   pos.w = square.getSize().w();
   pos.h = square.getSize().h();
-  if (type == APPLE)
-    SDL_SetRenderDrawColor(_rend, 0xFF, 0, 0, 0);
-  else if (type == SNAKE)
-    SDL_SetRenderDrawColor(_rend, 0xFF, 0xFF, 0xFF, 0x00);
+  SDL_SetRenderDrawColor(_rend, colors[0], colors[1], colors[2], 0x00);
   SDL_RenderDrawRect(_rend, &pos);
-  SDL_SetRenderDrawColor(_rend, 0x00, 0x00, 0x00, 0x00);
 
 }
 
 void Graphique::clearScreen()
 {
+  SDL_SetRenderDrawColor(_rend, 0x00, 0x00, 0x00, 0x00);
   SDL_RenderClear(_rend);
 }
 
@@ -91,3 +108,4 @@ void Graphique::updateEvent(EventHandler& eventHandler)
         }
     }
 }
+
