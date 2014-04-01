717ec56b6f09db435e1a85d424869031a1036c6e
collin_b <collin_b@epitech.eu>
2014-04-01 17:52:43 +0200

    add opengl -> just bug on square size

---
+++ lib_opengl/inc/Graphique.hpp
@@ -0,0 +1,29 @@
+#ifndef GRAPHIQUE_HPP_
+#define GRAPHIQUE_HPP_
+
+#include <iostream>
+#include <cstdlib>
+#include <map>
+
+#include "IGui.hpp"
+#include "Fault.hpp"
+
+class Graphique : public IGui
+{
+public:
+  Graphique();
+  virtual ~Graphique();
+
+  virtual void	createWindows(const Point2d<int>& size);
+  virtual void	drawSquare(const Box<int>& square, blockType type);
+  virtual void	affText(const Point2d<int>& pos, const std::stringstream& text);
+  virtual void	updateEvent(EventHandler& eventHandler);
+  virtual void	clearScreen();
+  virtual void	drawScreen();
+
+private:
+  EventType     _ev;
+  Point2d<int>	_win;
+};
+
+#endif
