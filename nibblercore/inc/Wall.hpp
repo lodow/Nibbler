1769ac5579a9d274caf23ac0901d7b41b022e3b6
moriss_h <moriss_h@epitech.eu>
2014-04-03 18:06:24 +0200

    Add wall.hpp

---
+++ nibblercore/inc/Wall.hpp
@@ -0,0 +1,14 @@
+#ifndef WALL_HPP_INCLUDED
+#define WALL_HPP_INCLUDED
+
+#include "Entity.hpp"
+
+class Wall : public Entity
+{
+public:
+  Wall();
+  ~Wall();
+private:
+};
+
+#endif // WALL_HPP_INCLUDED
